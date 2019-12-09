5df5131f433d71ab75787b9b0ce24d8dd9f01d6f
Karo <kar.hovhannisyan.95@gmail.com>
2019-12-09 19:57:39 +0400

    asdasdas

--- frontend/src/context/actions.js
+++
@@ -1,8 +0,0 @@
-export const ATTEMPT_LOGIN = 'ATTEMPT_LOGIN';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILED = 'LOGIN_FAILED';
-export const UPDATE_HISTORY = 'UPDATE_HISTORY';
-export const GET_VERIFEMAIL = 'GET_VERIFEMAIL';
-
-
-export const loginSuccess = () => ({type: LOGIN_SUCCESS});
